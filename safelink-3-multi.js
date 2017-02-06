17c7d3d28cd917eef25b53b0624aadb41c40d53f
jazuly <ajajazuly@gmail.com>
2017-02-06 19:04:35 +0700

    Update safelink-3-multi.js

--- safelink-3-multi.js
+++ safelink-3-multi.js
@@ -23,7 +23,7 @@ var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="android.html"}
 if(x=="2"){xxx="samsung.html"}
-if(x=="3"){xxx="xiaomi.html"}a_to_ve[i].href="http://safelinkv3.blogspot.co.id/2017/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="xiaomi.html"}a_to_ve[i].href="https://safelinkv3.blogspot.com/2017/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
